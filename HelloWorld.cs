20b756be22cc175d8892f47cddf9c8633dee6b17
Nathan Lemuel G. Santi <nahtan_itnas@yahoo.com>
2014-08-15 18:13:06 +0800

    Testing

---
+++ HelloWorld.cs
@@ -0,0 +1,13 @@
+#region Namespaces
+using System;
+using UnityEngine;
+#endregion // Namespaces
+
+public class HelloWorld : MonoBehaviour
+{
+	private void Start ()
+	{
+		Debug.Log("Hello World!");
+	}
+}
+
